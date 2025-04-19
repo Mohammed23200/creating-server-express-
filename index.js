03e569fb3f6a63fbb71b1d5067dedb018bf0156e
Mohammed23200 <mgsa112233@gmail.com>
2025-04-19 12:48:58 +0200

    basic router with express

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const app = express();
+// app.use((req,res)=>{
+//     console.log("we got re");
+//     res.send('<h1>this is h1 guys</h1>')
+// })
+// /cats =>meow
+// /dogs => 'woof'
+// ./ =>'welcome to our home'
+app.post("/cats", (req, res) => {
+    res.send("Hello World!");
+})
+app.get("/cats",(req,res)=>{
+    console.log("cat request");
+    res.send("Meow")
+})
+app.get("/dogs",(req,res)=>{
+    console.log("dog request");
+    res.send("woof")
+})
+app.get("/",(req,res)=>{
+    console.log("home request");
+    res.send("this is a home page")
+})
+app.get(/(.*)/,(req,res)=>{
+    res.send("404")
+})
+app.listen(3000,()=>{
+    console.log('Server started on port 3000');
+})
